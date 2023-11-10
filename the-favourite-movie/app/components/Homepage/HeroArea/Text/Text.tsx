cf41ef494b86c4d40c831395dbfb45a241afd0e8
Teresa Ferreira <teresa.ferreira@ltplabs.com>
2023-11-10 11:01:53 +0000

    create components for highlight text

--- the-favourite-movie/app/components/Homepage/HeroArea/Text/Text.tsx
+++ the-favourite-movie/app/components/Homepage/HeroArea/Text/Text.tsx
@@ -4,7 +4,7 @@ import { H1Title } from "~/components/UI/H1Title/H1Title";
 export const Text = () => {
   return (
       <>
-      <H1Title text="We’re changing the way people connect."/>
+      <H1Title text="Your favourite movie is here, with me ;)"/>
       <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
         Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in quis
         cupidatat mollit aute velit. Et labore commodo nulla aliqua proident
--- the-favourite-movie/app/components/Homepage/InfoList/InfoList.tsx
+++ the-favourite-movie/app/components/Homepage/InfoList/InfoList.tsx
@@ -3,11 +3,10 @@ import { H2Title } from "~/components/UI/H2Title/H2Title";
 
 export const InfoList = () => {
   return (
-    <div className="mx-auto max-w-2xl lg:mx-0 pb-16">
-      <H2Title text="Search for your favourite movie" />
+    <div className="mx-auto max-w-2xl lg:mx-0">
+      <H2Title text="Search for any movie" />
       <p className="mt-6 text-lg leading-8 text-gray-600">
-        We’re a dynamic group of individuals who are passionate about what we do
-        and dedicated to delivering the best results for our clients.
+        Et labore commodo nulla aliqua proident mollit ullamco exercitation tempor. Sint aliqua anim nulla sunt mollit id pariatur in voluptate cillum.
       </p>
     </div>
   );
